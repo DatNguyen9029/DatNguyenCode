7dcef926b4dab13d76d8f6fbd49f4832c0d53af0
Dat Nguyen <darlingg9029@gmail.com>
2025-10-28 22:57:38 +0700

    update test.c

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello world!!");
-
+	printf("Hello world!!\n");
+	int arr[6] = {2,3,5,7,6,8};
+	for(int i = 0; i < 6; i++) {
+		if(arr[i] % 2 == 0) {
+			printf("%d ", arr[i]);
+		}
+	}
 }
